41478c661afaa017ba9c0420d088b71d258b1bf9
gaoserx <143988398+gaoserx@users.noreply.github.com>
2023-09-18 21:11:43 +0800

    Add files via upload

--- my_strcmp.cpp
+++ my_strcmp.cpp
@@ -8,14 +8,17 @@ int my_strcmp(char* arr1, char* arr2)
 {
 	while (*(arr1++) == *(arr2++))
 	{
+		//两字符串相等返回0
 		if (*(arr1) == '\0')
 		{
 			return 0;
 		}
+		//前者小于后者返回-1
 		else if (arr1 < arr2)
 		{
 			return -1;
 		}
+		//前者大于后者返回1
 		else
 		{
 			return 1;
